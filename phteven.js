cf8485cdeb4f572651275487bb0b9b5b88b401a3
Stephen Koch <stephen.koch@gmail.com>
2016-11-18 16:56:17 -0500

    Heroku

--- phteven.js
+++ phteven.js
@@ -1,11 +1,18 @@
 var express = require('express');
-var sheetrock = require('sheetrock');
+// var bodyParser = require('body-parser');
 
 var app = express();
-app.set('port', (process.env.PORT || 5000));
-app.get('/', function(request, response) {
+app.set('port', (process.env.PORT || 3000));
 
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function(req, res) {
+    res.send('<img src="https://pbs.twimg.com/media/B_h8HN5WwAEC2rn.jpg">');
 });
+
+// app.post('/post-test', function(req, res) {
+// });
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
