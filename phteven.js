79843b83de49665b19fac1e9fc5188bcc582d18f
Stephen Koch <stephen.koch@gmail.com>
2016-11-21 15:31:42 -0500

    oauth

--- phteven.js
+++ phteven.js
@@ -11,6 +11,34 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //     res.send('<img src="https://pbs.twimg.com/media/B_h8HN5WwAEC2rn.jpg">');
 // });
 
+app.get('/slack-auth', function(req, res) {
+    let data = {form: {
+        client_id: '2151820749.106696824658',
+        client_secret: '1cd19097e7623950824df98bb5321c05',
+        code: req.query.code
+    }};
+
+    request.post('https://slack.com/api/oauth.access', data, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            // You are done.
+            // If you want to get team info, you need to get the token here
+            let token = JSON.parse(body).access_token; // Auth token
+
+            // Get the team domain name to redirect to the team URL after auth
+            request.post('https://slack.com/api/team.info', {form: {token: token}}, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    if(JSON.parse(body).error == 'missing_scope') {
+                        res.send('Phteven has been added to your team!');
+                    } else {
+                        let team = JSON.parse(body).team.domain;
+                        res.redirect('http://' +team+ '.slack.com');
+                    }
+                }
+            });
+        }
+    }
+});
+
 app.post('/', function(req, res) {
     // token
     // 2s5xgNw6FhEMblHYuvG14p5c
