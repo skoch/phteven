652e081f6b83f03705c01a647e6d5f180985f409
Stephen Koch <stephen.koch@gmail.com>
2016-11-21 12:13:30 -0500

    Heroku -- add body-parser

--- phteven.js
+++ phteven.js
@@ -1,18 +1,24 @@
 var express = require('express');
-// var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 
 var app = express();
 app.set('port', (process.env.PORT || 3000));
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function(req, res) {
-    res.send('<img src="https://pbs.twimg.com/media/B_h8HN5WwAEC2rn.jpg">');
-});
-
-// app.post('/post-test', function(req, res) {
+// app.get('/', function(req, res) {
+//     res.send('<img src="https://pbs.twimg.com/media/B_h8HN5WwAEC2rn.jpg">');
 // });
+
+app.post('/', function(req, res) {
+    // token
+    // 2s5xgNw6FhEMblHYuvG14p5c
+    if (!req.body) {
+        return res.sendStatus(400);
+    }
+    res.send('req.body' + req.body);
+});
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
